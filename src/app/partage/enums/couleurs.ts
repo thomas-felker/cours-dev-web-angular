c0aff333c2333af3e5d30d2446b6dd000f549ecf
unknown <thomas.felker8@etu.univ-lorraine.fr>
2024-01-23 14:52:38 +0100

    ajout enum

---
+++ src/app/partage/enums/couleurs.ts
@@ -0,0 +1,12 @@
+export enum Couleurs {
+  Rouge = "rouge",
+  Bleu = "bleu",
+  Vert = "vert",
+  Jaune = "jaune",
+  Orange = "orange",
+  Violet = "violet",
+  Rose = "rose",
+  Noir = "noir",
+  Blanc = "blanc",
+  Gris = "gris"
+}
